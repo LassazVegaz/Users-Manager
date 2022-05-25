7bda8c0e48f9edeb77c292a2a1fc3f6d58ed8f73
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-25 20:03:42 +0530

    db context added to IoC container

--- UsersManager/UsersManager.API/Program.cs
+++ UsersManager/UsersManager.API/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using UsersManager.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// JSON seetings
+var configs = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// add db context
+builder.Services.AddDbContext<UsersManagerContext>(o => o.UseMySql(
+    configs.GetConnectionString("UsersManagerContext"),
+    ServerVersion.Parse("8.0.29-mysql")));
 
 var app = builder.Build();
 
