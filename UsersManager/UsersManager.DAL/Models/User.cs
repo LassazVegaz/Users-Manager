337f76d79a704c15f206bac54653fd9eed02a9c7
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-25 19:51:09 +0530

    db scaffolded

---
+++ UsersManager/UsersManager.DAL/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsersManager.DAL.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Address { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public float Estimate { get; set; }
+    }
+}
---
+++ UsersManager/UsersManager.DAL/Models/UsersManagerContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UsersManager.DAL.Models
+{
+    public partial class UsersManagerContext : DbContext
+    {
+        public UsersManagerContext()
+        {
+        }
+
+        public UsersManagerContext(DbContextOptions<UsersManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;port=3306;database=users_manager;uid=root;pwd=SinSQL", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.HasIndex(e => e.Email, "user_unique_email")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Phone, "user_unique_phone")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(100)
+                    .HasColumnName("address");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Estimate).HasColumnName("estimate");
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(15)
+                    .HasColumnName("phone");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
