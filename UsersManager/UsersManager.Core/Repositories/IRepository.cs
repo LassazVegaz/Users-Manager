653916a80392d416509fe58f4bf7aa00d572bd20
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-25 20:25:10 +0530

    core layer and interfaces

---
+++ UsersManager/UsersManager.Core/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace UsersManager.Core.Repositories
+{
+    internal interface IRepository<TEntity>
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Search(Func<TEntity, bool> predicate);
+        TEntity Get(object id);
+        TEntity Create(TEntity entity);
+        TEntity Update(object id, TEntity entity);
+        void Delete(object id);
+    }
+}
---
+++ UsersManager/UsersManager.Core/Services/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using UsersManager.DAL.Models;
+
+namespace UsersManager.Core.Services
+{
+    internal interface IUsersService
+    {
+        User[] GetAllUsers();
+        User[] FindUsersByName(string username);
+        User CreateUser(User user);
+        User UpdateUser(int id, User user);
+        bool EmailExists(string email);
+        bool PhoneExists(string phone);
+    }
+}
