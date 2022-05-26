184a1629dc6ea943cb71d931ba8b2b9678c8e28d
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-26 19:41:51 +0530

    left right bars

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import { RightBarComponent } from './right-bar/right-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LeftBarComponent, RightBarComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent],
