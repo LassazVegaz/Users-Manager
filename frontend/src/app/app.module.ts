4b82d3ca41dc6e2550ce00ab9b50bfd3c17e256d
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-26 20:12:34 +0530

    left bar UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
 import { LeftBarComponent } from './left-bar/left-bar.component';
 import { RightBarComponent } from './right-bar/right-bar.component';
 
 @NgModule({
   declarations: [AppComponent, LeftBarComponent, RightBarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatGridListModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
